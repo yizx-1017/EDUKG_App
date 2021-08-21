6136d43be8a12bbf7cc0895e3227e49f9853a253
弋紫轩 <2019011307@secoder.net>
2021-08-21 17:49:54 +0800

    feat: add bottom navigation which has 4 attributes

---
+++ GKUDE/app/src/main/java/com/example/gkude/ui/user/UserFragment.java
@@ -0,0 +1,19 @@
+package com.example.gkude.ui.user;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+
+public class UserFragment extends Fragment{
+}
