19cae549b9487bd5120e3543fc8cbbe3e0f1ac6c
弋紫轩 <yizx-1017@github.com>
2021-09-10 00:53:52 +0800

    feat: add images to entity view

--- GKUDE/app/src/main/java/com/java/wangxingqi/adapter/ImageAdapter.java
+++ GKUDE/app/src/main/java/com/java/wangxingqi/adapter/ImageAdapter.java
@@ -1,2 +1,49 @@
-package com.java.wangxingqi.adapter;public class ImageAdapter {
+package com.java.wangxingqi.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class ImageAdapter extends PagerAdapter {
+    private Context mContext;
+    private List<String> imgList;
+
+    public ImageAdapter(Context context, List<String> imgList) {
+        this.mContext = context;
+        this.imgList = imgList;
+    }
+
+    @Override
+    public int getCount() {
+        return imgList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(ViewGroup container, int position){
+        ImageView imageView = new ImageView(mContext);
+        Glide.with(mContext)
+                .load(imgList.get(position))
+                .into(imageView);
+        container.addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ImageView) object);
+    }
 }
