ef3ca46f2a420320bebcda639d5c9a2e4ae900b9
弋紫轩 <yizx-1017@github.com>
2021-09-11 15:50:22 +0800

    fix: try to fix demicak

--- GKUDE/app/src/main/java/com/java/wangxingqi/StatsAnalyzeActivity.java
+++ GKUDE/app/src/main/java/com/java/wangxingqi/StatsAnalyzeActivity.java
@@ -7,13 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.formatter.StackedValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.java.wangxingqi.server.UserDataSource;
 import com.java.wangxingqi.server.UserRepository;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Objects;
@@ -31,6 +36,8 @@ public class StatsAnalyzeActivity extends AppCompatActivity {
         initToolbar();
         initData(entityPieChart, userRepository.getUser().getHistoryNum());
         initData(problemPieChart, userRepository.getUser().getWrongProblemNum());
+        entityPieChart.setCenterText("实体数据");
+        problemPieChart.setCenterText("错题数据");
     }
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.entity_toolbar);
@@ -45,20 +52,24 @@ public class StatsAnalyzeActivity extends AppCompatActivity {
         String[] coursesCN = new String[]{"语文", "英语", "数学", "物理", "化学", "生物", "历史", "地理","政治"};
         String[] coursesEN = new String[]{"chinese", "english", "math", "physics", "chemistry", "biology", "history", "politics", "geo"};
         int[] colors = {
-                Color.rgb(153, 195,217), Color.rgb(137,215,185)
+                Color.rgb(135,206,250), Color.rgb(137,215,185), Color.rgb(252,206,190),
+                Color.rgb(255,241,221), Color.rgb(221,214,202), Color.rgb(244,109,144),
+                Color.rgb(175,199,191), Color.rgb(238,223,218), Color.rgb(140,139,170)
         };
         for (int i = 0; i < coursesCN.length; i++) {
-            entities.add(new PieEntry(mp.get(coursesEN[i]), coursesCN[i]));
+            if (!mp.getOrDefault(coursesEN[i], 0).equals(0)) {
+                entities.add(new PieEntry(mp.getOrDefault(coursesEN[i], 0), coursesCN[i]));
+            }
         }
-        PieDataSet pieDataSet = new PieDataSet(entities, "实体数据");
-        // TODO: add more colors;
-        pieDataSet.setColors(ColorTemplate.PASTEL_COLORS);
-        pieDataSet.setValueTextColor(Color.WHITE);
-        pieDataSet.setValueTextSize(16f);
+        PieDataSet pieDataSet = new PieDataSet(entities, "");
+        pieDataSet.setColors(colors);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16);
 
         PieData pieData = new PieData(pieDataSet);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setEntryLabelColor(Color.BLACK);
         pieChart.setData(pieData);
-        pieChart.setCenterText("实体数据");
         pieChart.animate();
     }
 }
